e11f1d8cb19b55abcdad380a43d9d620ad0cf2f1
forsigner <forsigner@gmail.com>
2020-02-27 21:24:50 +0800

    feat(common): can InjectConfig

---
+++ packages/common/src/decorators/InjectConfig.ts
@@ -0,0 +1,20 @@
+import { Container } from 'typedi'
+import get from 'lodash.get'
+import { Application } from '../interfaces/Application'
+
+export function InjectConfig(key?: string) {
+  return function(object: Object, propertyName: string, index?: number) {
+    Container.registerHandler({
+      object,
+      propertyName,
+      index,
+      value: () => {
+        const app = Container.get<Application>('TIE_APP')
+        const config = app.config
+        if (!key) return config
+        // TODO: should check key
+        return get(config, key)
+      },
+    })
+  }
+}
