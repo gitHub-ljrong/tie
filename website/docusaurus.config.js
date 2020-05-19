2f91e540f7b7e4d4a2295deb08d2939753dbad08
forsigner <forsigner@gmail.com>
2020-05-19 23:43:23 +0800

    docs: upgrade docs

---
+++ website/docusaurus.config.js
@@ -0,0 +1,114 @@
+module.exports = {
+  title: 'TieJS',
+  tagline: 'A Node.js Framework based on Koa and TypeScript',
+  url: 'https://tie.js.org',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'tiejs',
+  projectName: 'tiejs.github.io',
+  themeConfig: {
+    sidebarCollapsible: false,
+    prism: {
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },
+    navbar: {
+      title: 'TieJS',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        {
+          to: 'docs/intro/quick-start',
+          activeBasePath: 'docs',
+          position: 'left',
+          label: '文档',
+        },
+        // { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://www.github.com/tiejs/tie',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/tiejs',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} forsigner`,
+    },
+  },
+  themes: ['@docusaurus/theme-live-codeblock'],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+      },
+    ],
+  ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+          remarkPlugins: [require('./src/plugins/remark-npm2yarn')],
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+}
---
+++ website/sidebars.js
@@ -0,0 +1,20 @@
+module.exports = {
+  someSidebar: {
+    简介: ['intro/quick-start', 'intro/about'],
+    基础: [
+      'basic/controller',
+      'basic/graphql',
+      'basic/provider',
+      'basic/config',
+      'basic/middleware',
+      'basic/plugin',
+    ],
+    进阶: [
+      'advance/development',
+      'advance/testing',
+      'advance/validation',
+      'advance/plugin',
+      'advance/typeorm',
+    ],
+  },
+}
