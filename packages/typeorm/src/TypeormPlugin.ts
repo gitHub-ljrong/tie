9d79a8bbd44b35e62c0f6527aee3720678f40063
forsigner <forsigner@gmail.com>
2020-07-01 20:08:35 +0800

    feat(typeorm): 支持EntityRepository

--- packages/typeorm/src/TypeormPlugin.ts
+++ packages/typeorm/src/TypeormPlugin.ts
@@ -1,5 +1,10 @@
 import { Injectable, IPlugin, Application, Container } from '@tiejs/common'
-import { createConnection, useContainer } from 'typeorm'
+import {
+  createConnection,
+  useContainer,
+  getMetadataArgsStorage,
+  getCustomRepository,
+} from 'typeorm'
 import { InjectLogger, Logger } from '@tiejs/logger'
 
 @Injectable()
@@ -8,6 +13,23 @@ export class TypeormPlugin implements IPlugin {
 
   async appDidReady(app: Application) {
     await this.connectDB(app)
+  }
+
+  private isEntityRepositories(service: any) {
+    const { entityRepositories = [] } = getMetadataArgsStorage()
+    return !!entityRepositories.find(i => i.target === service.type)
+  }
+
+  // 黑科技
+  private hackEntityRepository() {
+    const container: any = Container.of(undefined) || {}
+    const { services = [] } = container
+
+    for (const service of services) {
+      if (this.isEntityRepositories(service)) {
+        service.value = getCustomRepository(service.type)
+      }
+    }
   }
 
   private async connectDB(app: Application) {
@@ -19,8 +41,8 @@ export class TypeormPlugin implements IPlugin {
 
     try {
       await createConnection(config)
-
       this.logger.info('database connect success')
+      this.hackEntityRepository()
     } catch (error) {
       this.logger.error('database connect fail')
       this.logger.error(error)
